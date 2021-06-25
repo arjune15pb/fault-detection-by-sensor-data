23b58550eecaf1d85fca693272a253bef1d6eea2
ARJUN KUMAR <karnarjun2000@gmail.com>
2021-06-25 23:08:02 +0530

    Update predictFromModel.py

--- predictFromModel.py
+++ predictFromModel.py
@@ -15,12 +15,14 @@ class prediction:
             self.pred_data_val = Prediction_Data_validation(path)
 
     def predictionFromModel(self):
+        print("Prediction started")
 
         try:
             self.pred_data_val.deletePredictionFile() #deletes the existing prediction file from last run!
             self.log_writer.log(self.file_object,'Start of Prediction')
             data_getter=data_loader_prediction.Data_Getter_Pred(self.file_object,self.log_writer)
             data=data_getter.get_data()
+            print("deleted prediction file")
 
             #code change
             # wafer_names=data['Wafer']
@@ -33,6 +35,7 @@ class prediction:
 
             cols_to_drop=preprocessor.get_columns_with_zero_std_deviation(data)
             data=preprocessor.remove_columns(data,cols_to_drop)
+            print("Preprocessing done")
             #data=data.to_numpy()
             file_loader=file_methods.File_Operation(self.file_object,self.log_writer)
             kmeans=file_loader.load_model('KMeans')
@@ -40,23 +43,32 @@ class prediction:
             ##Code changed
             #pred_data = data.drop(['Wafer'],axis=1)
             clusters=kmeans.predict(data.drop(['Wafer'],axis=1))#drops the first column for cluster prediction
+            print("cluster created")
             data['clusters']=clusters
             clusters=data['clusters'].unique()
+            print("Now travelling to each cluster set")
             for i in clusters:
+                print("inside a cluster")
                 cluster_data= data[data['clusters']==i]
                 wafer_names = list(cluster_data['Wafer'])
                 cluster_data=data.drop(labels=['Wafer'],axis=1)
                 cluster_data = cluster_data.drop(['clusters'],axis=1)
                 model_name = file_loader.find_correct_model_file(i)
+                print("model loaded")
                 model = file_loader.load_model(model_name)
                 result=list(model.predict(cluster_data))
+                print("prediction of this cluster done")
                 result = pandas.DataFrame(list(zip(wafer_names,result)),columns=['Wafer','Prediction'])
+                print("zipped with wafer name")
                 path="Prediction_Output_File/Predictions.csv"
                 result.to_csv("Prediction_Output_File/Predictions.csv",header=True,mode='a+') #appends result to prediction file
+                print("prediction push into file")
             self.log_writer.log(self.file_object,'End of Prediction')
+            print("Now log created")
         except Exception as ex:
             self.log_writer.log(self.file_object, 'Error occured while running the prediction!! Error:: %s' % ex)
             raise ex
+        print("all done")
         return path, result.head().to_json(orient="records")
 
 
